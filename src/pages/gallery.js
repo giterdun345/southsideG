f142f55d1cba99b38d9ee586fd10271241c46b8a
John Ketterer <johnmketterer@gmail.com>
2021-05-16 15:56:08 -0500

    cloudinary linked

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,13 +1,51 @@
 import React from 'react'
 import Seo from "../components/layout/seo"
+import {useStaticQuery, graphql} from 'gatsby';
+
 
 import Layout from "../components/layout/layout"
 
 const Gallery = () => {
+  const data = useStaticQuery(
+    graphql`
+    query CloudinaryImage {
+        allCloudinaryMedia {
+        edges {
+            node {
+                secure_url
+                context {
+                    custom {
+                        alt
+                        caption
+                    }
+                }
+                resource_type
+            }
+        }
+      }
+    }`
+);
+
+const images = data.allCloudinaryMedia.edges;
+
+
   return ( 
     <Layout>
       <Seo title="Gallery" />
-    <div> Past WOrk IMage gallery</div>
+    
+      <div className="container">
+            {images.map((image, index) => (
+                <figure className="wave" key={`${index}-image`}>
+                    <img 
+                        src={image.node.secure_url} 
+                        alt={image.node.context.custom.alt} >
+                        className={}
+                    </img>
+                    <figcaption>{image.node.context.custom.caption}</figcaption>
+                </figure>
+                ))
+            }
+        </div>
     </Layout>
    );
 }
