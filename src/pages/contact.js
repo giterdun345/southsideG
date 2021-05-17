95c5ec3ebd6a418e5d66a7f55195ac41c5ae3407
John Ketterer <johnmketterer@gmail.com>
2021-05-17 11:18:38 -0500

    fixing contact form submission

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,14 +6,12 @@ import Layout from "../components/layout/layout"
 import ContactForm from '../components/contact/contactForm'
 import * as ctSty from '../components/contact/contact.module.scss'
 
-
 const Contact = () => {
   return ( 
     <Layout>
       <Seo title="Contact" />
       <h1 className={ctSty.intro}>Contact us for a free consultation</h1>
-    
-    <ContactForm/>
+      <ContactForm/>
     </Layout>
    );
 }
