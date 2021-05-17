9bc6dda9d5bfb1aec29f66001a62a52346c4c674
John Ketterer <johnmketterer@gmail.com>
2021-05-16 20:10:58 -0500

    altered styling on extras

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Seo from "../components/layout/seo"
+import { StaticImage } from 'gatsby-plugin-image'
+import {Link} from 'gatsby'
 
 import Layout from "../components/layout/layout"
 
